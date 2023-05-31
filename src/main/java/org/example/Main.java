63de42f510aeb0168a9802b59aad8c3293ed04ae
LorTho <134836725+LorTho@users.noreply.github.com>
2023-05-31 14:28:49 +0200

    methoden und schleifen

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,38 @@
+package org.example;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(addition(2, 3));
+        System.out.println(summe(2, 3));
+        System.out.println(summe2(2, 3));
+        System.out.println(fakultyFor(7));
+        System.out.println(fakultyWhile(7));
+
+       // scannerSumme();
+    }
+    public static int addition(int x, int y){
+        return x+y;
+    }
+    public static String summe(int x, int y){
+        return "Summe: " + (x+y);
+    }
+    public static String summe2(int x, int y){
+        return String.format("Summe: %d + %d = %d",x, y, (x+y));
+    }
+    public static String fakultyFor(int x){
+        int f = 1;
+        for(int i=1;i<=x;i++){
+            f = f * i;
+        }
+        return String.format("For: Die Fakulät von %d ist: %d" ,x,f);
+    }
+    public static String fakultyWhile(int x){
+        int f=1, count=1;
+        while(count<=x){
+            f=f * count;
+            count++;
+        }
+        return String.format("While: Die Fakulät von %d ist: %d" ,x,f);
+    }
+}
